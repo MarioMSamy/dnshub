f7764e5c875c315494f3729830a67e77a1e637f5
vtremblay <vinz19@gmail.com>
2016-05-17 21:27:10 -0400

    ref

--- app/dns.js
+++ app/dns.js
@@ -5,12 +5,6 @@ class DNS {
     constructor() {}
 
     parse(data) {
-        let label = new Parser()
-            .uint8('length')
-            .string('label', {
-                encoding: 'utf8',
-                length: 'length'
-            });
 
         let message = new Parser()
             .nest('header', {
@@ -32,19 +26,30 @@ class DNS {
                     .uint16('authority')
                     .uint16('additional')
             })
-            .array('labels', {
-                type: label,
-                readUntil: function(item, buffer) {
-                    return item.length === 0
-                }
-            })
-            .string('questionType', {
-                encoding: 'hex',
-                length: 2
-            })
-            .uint16('class');
+            .array('questions', {
+                type: new Parser()
+                    .array('labels', {
+                        type: new Parser()
+                            .uint8('length')
+                            .string('label', {
+                                encoding: 'utf8',
+                                length: 'length'
+                            }),
+                        readUntil: function(item, buffer) {
+                            return item.length === 0
+                        }
+                    })
+                    .string('questionType', {
+                        encoding: 'hex',
+                        length: 2
+                    })
+                    .uint16('class'),
+                length: 'header.question'
+            });
 
+        console.time("parse")
         console.log(JSON.stringify(message.parse(data)));
+        console.timeEnd("parse")
     }
 }
 
