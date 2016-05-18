0a3b1cb76a2be53d9d3874e83a44edea91d6dca6
Vincent Tremblay <vincent.tremblay@pj.ca>
2016-05-18 07:37:16 -0400

    Keep only main DNS

--- app/dashboard/dashboard.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'hub-dashboard',
-    templateUrl: 'app/dashboard/dashboard.component.html'
-})
-export class DashboardComponent {}
